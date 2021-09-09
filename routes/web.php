4004f7046abad44c63cdb0411f6aa6a7e84d9ff1
Hossam Hassan <hossamhassan889@gmail.com>
2021-09-09 18:02:08 +0200

    trans

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-Route::redirect('/', '/login');
+// Route::redirect('/', '/login');
+
+Route::get('/' , function(){
+    return view('land');
+});
 Route::get('/home', function () {
     if (session('status')) {
         return redirect()->route('admin.home')->with('status', session('status'));
